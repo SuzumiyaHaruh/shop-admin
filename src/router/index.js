135d80834bda26b6e1fbad9ad78374f0782fc77f
凉宫春日 <3563692396@qq.com>
2022-09-21 13:00:08 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import {createRouter, createWebHashHistory} from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('@/pages/index.vue')
+    },
+    // 将匹配所有内容并将其放在 `$route.params.pathMatch` 下
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import('@/pages/404.vue')
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
